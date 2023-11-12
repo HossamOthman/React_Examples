c9bd02736eae8657fccf2c2db3bda4d58a9e7bce
Hossam Othman <rowheadrex@gmail.com>
2023-11-12 23:26:50 +0100

    form & event

--- inventory_management/src/App.js
+++ inventory_management/src/App.js
@@ -1,47 +1,24 @@
-import Info from './info.js'
-import {useState} from 'react'
+import "./App.css";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  }
+
   return (
     <div className="App">
-      <p>Bare Bones App</p>
-      <Info title="inventory"/>
-      <ButtonState />
-
+      <SearchBar callback={updateData}/>
+      <p>Name: {"name" in data ? data["name"] : "No Data to Display"}</p>
+      <p>Max Price: {"price" in data ? data["price"] : "No Data to Display"}</p>
+      <p>Type: {"type" in data ? data["type"] : "No Data to Display"}</p>
+      <p>Brand: {"brand" in data ? data["brand"] : "No Data to Display"}</p>
     </div>
   );
 }
 
-function ButtonState(){
-  const [title, setTitle] = useState('');
-  const [count, setCount] = useState(0);
-
-  const updateTitleOnClick = () => {
-    setTitle('we have a title fom use state')
-  }
-
-  const updateCounterOnClick = () => {
-    setCount(count + 1)
-  }
-
-  return(
-    <>
-    <Data title={title} count={count}/>
-    <button onClick={updateTitleOnClick}>update Title</button>
-    <button onClick={updateCounterOnClick}>update Counter</button>
-    </>
-    
-  )
-}
-
-
-function Data(props) {
-  return (
-    <div>
-    <p>Title: {props.title} </p>
-    <p>Counter: {props.count}</p>
-    </div>
-  )
-}
 
 export default App;
