a4f051520f140e82f1d926d8329d2983aa08ea31
Hossam Othman <rowheadrex@gmail.com>
2023-11-15 13:38:46 +0100

    add todo

--- 03_ToDo_kyle/src/App.jsx
+++ 03_ToDo_kyle/src/App.jsx
@@ -1,33 +1,50 @@
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
+
+
 
 export default function App() {
- return (
-  <>
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item"></input>
-      </div>
-      <button className="btn">Add</button>
-    </form>
-    <h1 className="header">ToDo List</h1>
-    <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox"></input>
-          item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox"></input>
-          item 2
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-    </ul>
-  </>
-          
-        )
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false}]
+    })
+
+    setNewItem("")
+  }
+
+  console.log(todos)
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          ></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">ToDo List</h1>
+      <ul className="list">
+        {todos.map(todo => {
+          return  <li key={todo.id}>
+                    <label>
+                      <input type="checkbox" checked={todo.completed}
+                      ></input>
+                      {todo.title}
+                    </label>
+                    <button className="btn btn-danger">Delete</button>
+                  </li>
+        })}
+      </ul>
+    </>
+  );
 }
