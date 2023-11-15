adbba952d5dec87a212082c7c95f62c89a86047a
Hossam Othman <rowheadrex@gmail.com>
2023-11-15 13:56:42 +0100

    toggle check & delete

--- 03_ToDo_kyle/src/App.jsx
+++ 03_ToDo_kyle/src/App.jsx
@@ -17,7 +17,24 @@ export default function App() {
     setNewItem("")
   }
 
-  console.log(todos)
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -34,14 +51,15 @@ export default function App() {
       </form>
       <h1 className="header">ToDo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return  <li key={todo.id}>
                     <label>
-                      <input type="checkbox" checked={todo.completed}
-                      ></input>
+                      <input  type="checkbox" checked={todo.completed}
+                              onChange={e => toggleTodo(todo.id, e.target.checked)}></input>
                       {todo.title}
                     </label>
-                    <button className="btn btn-danger">Delete</button>
+                    <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
                   </li>
         })}
       </ul>
