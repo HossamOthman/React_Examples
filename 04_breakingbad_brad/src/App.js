991f1c4f8c679c53ecfedd32e95ac91f11b824db
Hossam Othman <rowheadrex@gmail.com>
2023-11-15 17:51:57 +0100

    bare bones

---
+++ 04_breakingbad_brad/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/ui/Header'
+
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
