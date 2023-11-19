642380a52cbfc30dc47e841520120beb09fbfa16
Hossam Othman <rowheadrex@gmail.com>
2023-11-19 22:21:34 +0100

    use effect http req

--- 04_breakingbad_brad/src/App.js
+++ 04_breakingbad_brad/src/App.js
@@ -1,12 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import HeaderBB from './components/ux/HeaderBB'
 import './App.css';
-import Header from './components/ui/Header'
+
 
 
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://api.github.com/users`)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
-      <Header />
+      <HeaderBB/>
     </div>
   );
 }
